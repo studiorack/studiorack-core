1da3f58a2a3810d555258d069ddb3edf24b9b014
Kim T <kimturley@gmail.com>
2020-11-13 19:13:18 -0800

    Example file tests

--- tests/file.test.ts
+++ tests/file.test.ts
@@ -19,6 +19,9 @@ import {
 } from '../src/file';
 
 const DIR_PATH = './tests/new-directory';
+const DIR_RENAME = './tests/new-directory-renamed';
+
+const FILE_PATH = './tests/new-directory/file.txt';
 
 test('Create new directory', () => {
   expect(dirCreate(DIR_PATH)).toBe(DIR_PATH);
@@ -26,4 +29,32 @@ test('Create new directory', () => {
 
 test('Create existing directory', () => {
   expect(dirCreate(DIR_PATH)).toBe(false);
+});
+
+test('Directory is empty', () => {
+  expect(dirEmpty(DIR_PATH)).toBe(true);
+});
+
+test('Directory exists', () => {
+  expect(dirExists(DIR_PATH)).toBe(true);
+});
+
+test('Read directory', () => {
+  expect(dirRead(DIR_PATH)).toMatchObject([DIR_PATH]);
+});
+
+test('Create file', () => {
+  expect(fileCreate(FILE_PATH, 'file contents')).toBeUndefined();
+});
+
+test('Rename directory', () => {
+  expect(dirRename(DIR_PATH, DIR_RENAME)).toBeUndefined();
+});
+
+test('Delete existing directory', () => {
+  expect(dirDelete(DIR_RENAME)).toBeUndefined();
+});
+
+test('Delete missing directory', () => {
+  expect(dirDelete(DIR_RENAME)).toBe(false);
 });
