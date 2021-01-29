06bdf4fc5ecf96fb66b9c2942447c5b10436e3eb
Kim T <kimturley@gmail.com>
2021-01-29 12:06:48 -0800

    Export project types

--- src/projects.ts
+++ src/projects.ts
@@ -3,19 +3,20 @@ import { idToSlug, pathGetId, pathGetRepo } from './utils';
 import { Project } from './types';
 import os from 'os';
 import path from 'path';
+import slugify from 'slugify';
 
 // List of notable DAWs
 // https://en.wikipedia.org/wiki/Digital_audio_workstation#List_of_notable_commercial_DAWs
 let PROJECT_ROOT = '.';
-const PROJECT_LOCAL = `/**/*.{als,cpr,flp,logic,ptx,rpp}`;
 const PROJECT_TYPES: { [property: string]: string } = {
-  '.als': 'ableton',
-  '.cpr': 'cubase',
-  '.flp': 'fl-studio',
-  '.logic': 'logic',
-  '.ptx': 'pro-tools',
-  '.rpp': 'reaper'
+  'als': 'Ableton',
+  'cpr': 'Cubase',
+  'flp': 'FL Studio',
+  'logic': 'Logic',
+  'ptx': 'Pro Tools',
+  'rpp': 'Reaper'
 }
+const PROJECT_LOCAL = `/**/*.{${Object.keys(PROJECT_TYPES).join(',')}}`;
 
 function projectCreateJson(pathItem: string): Project {
   return {
@@ -24,7 +25,7 @@ function projectCreateJson(pathItem: string): Project {
     "name": path.basename(pathItem, path.extname(pathItem)),
     "description": "Created using StudioRack",
     "tags": [
-      PROJECT_TYPES[path.extname(pathItem)]
+      PROJECT_TYPES[path.extname(pathItem).replace('.', '')]
     ],
     "version": "1.0.0",
     "date": "2000-01-01T00:00:00.000Z",
@@ -65,7 +66,7 @@ async function projectsGet() {
       return;
     }
     const jsonPath = projectPath.substring(0, projectPath.lastIndexOf('.')) + '.json';
-    const relativePath = projectPath.replace('./test/', '').replace('.als', '');
+    const relativePath = projectPath.replace(PROJECT_ROOT + '/', '');
     let project = fileJsonLoad(jsonPath);
     if (!project) {
       project = projectCreateJson(projectPath);
@@ -80,7 +81,7 @@ async function projectsGet() {
     project.id = `${pathGetRepo(relativePath)}/${pathGetId(relativePath)}`;
     project.path = projectPath;
     project.slug = idToSlug(project.id);
-    project.type = PROJECT_TYPES[path.extname(projectPath)];
+    project.type = slugify(PROJECT_TYPES[path.extname(projectPath).replace('.', '')], { lower: true });
     fileJsonCreate(jsonPath, project);
     list.push(project);
   });
@@ -95,6 +96,7 @@ function projectRoot(dir?: string) {
 }
 
 export {
+  PROJECT_TYPES,
   projectGet,
   projectsGet,
   projectRoot
