41bc0054895531be4c6f2c312f6ea293322f5d06
Kim T <kimturley@gmail.com>
2022-01-16 21:08:11 -0800

    Fix import bug

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { ConfigInterface } from './types/config';
 import { dirPlugins, dirProjects, fileDelete, fileJsonCreate, fileJsonLoad } from './file';
-import { configDefaults } from '.';
+import { configDefaults } from './config-defaults';
 
 const CONFIG_FILE_PATH = __dirname + '/config.json';
 const config: ConfigInterface = fileJsonLoad(CONFIG_FILE_PATH) || configDefaults();
