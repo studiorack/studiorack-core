ddb75e9b5430de661c2e082046dcbe32e1277e8e
Kim T <kimturley@gmail.com>
2022-04-06 18:55:49 -0700

    Run studiorack/cli instead of node script

--- src/admin.ts
+++ src/admin.ts
@@ -1,58 +1,30 @@
 // Run when Electron needs elevated privileges
-// npm run build && node ./dist/admin.js --operation install --id studiorack/adlplug/adlplug
-// npm run build && node ./dist/admin.js --operation uninstall --id studiorack/adlplug/adlplug
+// studiorack plugin install studiorack/adlplug/adlplug
+// studiorack plugin uninstall studiorack/adlplug/adlplug
 
 import sudoPrompt from '@vscode/sudo-prompt';
-import path from 'path';
-import { dirAppData } from './file';
-import { pluginInstall, pluginUninstall } from './plugin';
 
-interface Arguments {
-  operation: string;
-  id: string;
-  ver: string;
-}
-
-function getArguments(): Arguments {
-  return {
-    operation: process.argv[3],
-    id: process.argv[5],
-    ver: process.argv[7],
-  };
-}
-
-async function init() {
-  const argv: Arguments = getArguments();
-  if (argv.operation === 'install') {
-    await pluginInstall(argv.id, argv.ver);
-  } else if (argv.operation === 'uninstall') {
-    await pluginUninstall(argv.id, argv.ver);
-  }
-}
-
-function runCliAsAdmin(args: string, isDependency?: boolean): Promise<string> {
+function runCliAsAdmin(args: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
-    const dirPath: string = __dirname.includes('.asar') ? __dirname.replace('.asar', '.asar.unpacked') : __dirname;
-    const fullPath: string = isDependency
-      ? path.join('node_modules', '@studiorack', 'core', 'dist', 'admin.js')
-      : path.join(dirPath, 'admin.js');
-    console.log(`node ${fullPath} ${args}`);
-    sudoPrompt.exec(`node ${fullPath} ${args}`, { name: 'StudioRack' }, (error, stdout, stderr) => {
-      if (stdout) {
-        console.log('runCliAsAdmin', stdout);
+    console.log(`studiorack ${args}`);
+    sudoPrompt.exec(
+      `npm install @studiorack/cli -g; studiorack ${args}`,
+      { name: 'StudioRack' },
+      (error, stdout, stderr) => {
+        if (stdout) {
+          console.log('runCliAsAdmin', stdout);
+        }
+        if (stderr) {
+          console.log('runCliAsAdmin', stderr);
+        }
+        if (error) {
+          reject(error);
+        } else {
+          resolve(stdout?.toString() || '');
+        }
       }
-      if (stderr) {
-        console.log('runCliAsAdmin', stderr);
-      }
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout?.toString() || '');
-      }
-    });
+    );
   });
 }
-
-init();
 
 export { runCliAsAdmin };
