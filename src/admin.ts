b51e8c5d806d9c236e6b448e2f4479fd927a75d2
Kim T <kimturley@gmail.com>
2022-04-27 20:51:07 -0700

    Format code

--- src/admin.ts
+++ src/admin.ts
@@ -17,36 +17,44 @@ function isCliInstalled() {
 function installCli(): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     console.log(`npm install @studiorack/cli -g`);
-    sudoPrompt.exec(`npm install @studiorack/cli -g`, {
-      name: 'StudioRack',
-      icns: './src/images/icon.icns',
-    }, (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.log('installCli.error', error, stderr);
-        reject(error);
-      } else {
-        console.log('installCli.success', stdout);
-        resolve(stdout?.toString() || '');
+    sudoPrompt.exec(
+      `npm install @studiorack/cli -g`,
+      {
+        name: 'StudioRack',
+        icns: './src/images/icon.icns',
+      },
+      (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.log('installCli.error', error, stderr);
+          reject(error);
+        } else {
+          console.log('installCli.success', stdout);
+          resolve(stdout?.toString() || '');
+        }
       }
-    });
+    );
   });
 }
 
 function runCliAsAdmin(args: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     console.log(`studiorack ${args}`);
-    sudoPrompt.exec(`studiorack ${args}`, {
-      name: 'StudioRack',
-      icns: './src/images/icon.icns',
-    }, (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.log('runCliAsAdmin.error', error, stderr);
-        reject(error);
-      } else {
-        console.log('runCliAsAdmin.success', stdout);
-        resolve(stdout?.toString() || '');
+    sudoPrompt.exec(
+      `studiorack ${args}`,
+      {
+        name: 'StudioRack',
+        icns: './src/images/icon.icns',
+      },
+      (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.log('runCliAsAdmin.error', error, stderr);
+          reject(error);
+        } else {
+          console.log('runCliAsAdmin.success', stdout);
+          resolve(stdout?.toString() || '');
+        }
       }
-    });
+    );
   });
 }
 
