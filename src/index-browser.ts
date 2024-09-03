aa852d47647882a9636bcf24b0affabe964121fa
Kim T <kimturley@gmail.com>
2024-09-03 23:08:14 +0100

    Browser-specific module export

---
+++ src/index-browser.ts
@@ -0,0 +1,14 @@
+// Commented out imports are not browser compatible.
+export * from './api.js';
+// export * from './config.js';
+export * from './config-defaults.js';
+// export * from './file.js';
+// export * from './plugin.js';
+// export * from './project.js';
+export * from './types/config.js';
+export * from './types/daw.js';
+export * from './types/plugin.js';
+export * from './types/project.js';
+export * from './types/tool.js';
+// export * from './tool.js';
+export * from './utils.js';
