44dce81229cb468fc44a045ae6271f81cda71641
Kim T <kimturley@gmail.com>
2024-10-14 20:01:45 -0700

    Fix eslint glob path

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,6 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-  {
-    ignores: ['build', 'src/convert.ts'],
-  },
-];
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  ignores: ['build'],
+});
